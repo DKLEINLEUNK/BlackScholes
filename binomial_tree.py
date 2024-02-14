dedb3ff081f6b3326b8678e68b8cdc57fb13bb02
DKLEINLEUNK <d.kleinleunk@gmail.com>
2024-02-14 13:46:38 +0100

    Added payoffUS

--- binomial_tree.py
+++ binomial_tree.py
@@ -36,37 +36,62 @@ def buildTree(S, vol , T, N):
     return matrix
 
 
-def valueOptionBinomial(tree , T, r , K, vol, return_tree=False):
+def valueOptionBinomial(tree, T, r , K, vol, return_tree=False):
 
-    N = tree.shape[1] - 1 #Getting N from the number of columns - 1
+    N = tree.shape[1] - 1  # finds N from the number of columns - 1
 
     dt = T / N
-    u = np.exp(vol*np.sqrt(dt)) #According to formula derived in appendix
+    u = np.exp(vol*np.sqrt(dt))
     d = np.exp(-vol*np.sqrt(dt)) 
-    p = (np.exp(r*dt) - d)/(u-d) #According to formula derived in appendix
+    p = (np.exp(r*dt) - d)/(u-d)
     columns = tree.shape[1] 
     rows = tree.shape[0]
     
-    # Walk backward , we start in last row of the matrix
-    # Add the payoff function in the last row for c in np.arange(columns):
+    payoff = np.zeros_like(tree)
 
-    payoff = np.zeros_like(tree)  # ASK TA IF THIS IS OKAY
-
-    for c in np.arange(columns): #Loop over columns
-
-        S = tree[rows - 1, c] #Getting the first stock price in the last row
+    for c in np.arange(columns):  # loops over columns
+        S = tree[rows - 1, c]  # finds first stock price in the last row
         payoff[rows - 1, c] = max(0, S-K)
-        
-        #TODO #Calculating the payoff of the option using S
-    
-    # For all other rows , we need to combine from previous rows 
-    # We walk backwards , from the last row to the first row
-    for i in np.arange(rows - 1)[::-1]: #Loop over the row in reverse order
-        for j in np.arange(i+1): # Loop over columns 
+            
+    for i in np.arange(rows - 1)[::-1]:  # loops over the row in reverse order
+        for j in np.arange(i+1):  # loops over columns 
             down = payoff[i+1, j]
             up = payoff[i+1,j+1]
-
             payoff[i,j] = np.exp(-r*dt)*(p*up + (1-p)*down)
+    
+    if return_tree:
+        return payoff
+    
+    return payoff[0][0]
+
+
+def valueUSOptionBinomial(tree, T, r , K, vol, exercise_timestep, return_tree=False):
+    
+    N = tree.shape[1] - 1  # finds N number of timesteps
+
+    dt = T / N
+    u = np.exp(vol*np.sqrt(dt))
+    d = np.exp(-vol*np.sqrt(dt))
+    p = (np.exp(r*dt) - d)/(u-d)
+    
+    row_of_interest = exercise_timestep + 1  # returns the relevant payoff states 
+    columns_of_interest = np.arange(row_of_interest)  # finds the column indices at early exercise
+    
+    payoff = np.zeros_like(tree)
+
+    for col in columns_of_interest:
+        S = tree[exercise_timestep, col]
+        payoff[exercise_timestep, col] = max(0, S-K)
+
+    row_to_update = row_of_interest - 1
+
+    for row in np.arange(row_to_update)[::-1]:  # loops over the row to update in reverse order
+        for col in np.arange(row + 1):  # loops over columns corresponding to the row 
+            
+            down = payoff[row + 1, col]    # gets the next down-state
+            up = payoff[row + 1, col + 1]  # gets the next up-state
+            
+            payoff[row, col] = np.exp(-r*dt)*(p*up + (1-p)*down)
     
     if return_tree:
         return payoff
@@ -82,10 +107,30 @@ if __name__ == '__main__':
     r = 0.06     # interest rate
     S_0 = 100    # stock price at t = 0
     sigma = 0.2  # volatility
-    N = 50       # timesteps
+    N = 5        # timesteps
 
-    tree = buildTree(S_0, sigma, T, N)
-    payoff = valueOptionBinomial(tree, T, r, K, sigma, return_tree=False)
+    tree = buildTree(S_0, sigma, T, 3)
+    payoff = valueOptionBinomial(
+        tree, 
+        T, 
+        r, 
+        K, 
+        sigma, 
+        return_tree=True
+    )
 
+    treeUS = buildTree(S_0, sigma, T, 3)
+    payoffUS = valueUSOptionBinomial(
+        treeUS, 
+        T, 
+        r, 
+        K, 
+        sigma, 
+        exercise_timestep=3, 
+        return_tree=True
+    )
+    
+    print(payoff)
+    print(payoffUS)
     # approximate_value = payoff[0][0]
-    print(f'Value of the option: {payoff:.2f}')
+    # print(f'Value of the option: {payoff:.2f}')
